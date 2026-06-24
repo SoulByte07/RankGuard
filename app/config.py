0816f7c43dd673522a36f3c012b20d9393eb84f1
soulbyte07 <soul3147@proton.me>
2026-06-24 10:46:46 +0530

    config.py: + read database url

---
+++ app/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    database_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/rankgard"
+
+    model_config = {"env_file": ".env", "env_file_encoding": "utf-8"}
+
+
+settings = Settings()
