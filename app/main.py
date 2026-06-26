53feae526f913ed1e2117494ce0cc3e51e890f97
soulbyte07 <soul3147@proton.me>
2026-06-26 16:05:10 +0530

    main.py: + summary router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from app.database import engine
+from app.routers.summary import router as summary_router
 from app.routers.transaction import router as transaction_router
 
 app = FastAPI(title="RankGuard")
 app.include_router(transaction_router)
+app.include_router(summary_router)
 
 
 @app.on_event("shutdown")
