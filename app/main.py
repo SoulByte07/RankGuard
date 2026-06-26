dc4bda805eef6a3f9d72c5632c7e8b7bf3601af6
soulbyte07 <soul3147@proton.me>
2026-06-26 16:45:35 +0530

    main: + ranking rounter

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
 from app.database import engine
+from app.routers.ranking import router as ranking_router
 from app.routers.summary import router as summary_router
 from app.routers.transaction import router as transaction_router
 
 app = FastAPI(title="RankGuard")
 app.include_router(transaction_router)
 app.include_router(summary_router)
+app.include_router(ranking_router)
 
 
 @app.on_event("shutdown")
